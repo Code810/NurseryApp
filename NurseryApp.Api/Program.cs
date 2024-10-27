2fe8c363384d283fac43757cb79c1567f2fbe167
code810 <nadirssh@code.edu.az>
2024-10-27 20:54:12 +0400

    do not migrate

--- NurseryApp.Api/Program.cs
+++ NurseryApp.Api/Program.cs
@@ -1,8 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using NurseryApp.Api;
 using NurseryApp.Api.Hubs;
 using NurseryApp.Api.Middlewares;
-using NurseryApp.Data.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +20,11 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<NurseryAppContext>();
-        dbContext.Database.Migrate();
-    }
+    //using (var scope = app.Services.CreateScope())
+    //{
+    //    var dbContext = scope.ServiceProvider.GetRequiredService<NurseryAppContext>();
+    //    dbContext.Database.Migrate();
+    //}
 }
 
 app.UseHealthChecks("/healthy");
