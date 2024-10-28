6c930dd23b51da4fb142b7dd57d851a0ea77c7d8
code810 <nadirssh@code.edu.az>
2024-10-28 21:23:09 +0400

    test sawagger

--- NurseryApp.Api/Program.cs
+++ NurseryApp.Api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.Register(config);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
